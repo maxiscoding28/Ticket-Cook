8f32fa8b8b53355e55a05e00fe9374c41789e996
max <maxtwinslow1@gmail.com>
2024-04-20 12:22:31 -0700

    x

--- cmd/initRecipe.go
+++ cmd/initRecipe.go
@@ -20,14 +20,5 @@ var initRecipeCmd = &cobra.Command{
 
 func init() {
 	recipeCmd.AddCommand(initRecipeCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initRecipeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initRecipeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Prevent creating a default recipe
 }
